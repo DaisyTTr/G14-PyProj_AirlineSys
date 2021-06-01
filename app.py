044e84b2830637d8ac2c5736273fb1b41de1038f
DaisyTTr <ngttrango@gmail.com>
2021-06-02 01:43:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,76 +12,65 @@ user = []
 
 app.title("Flight Reservation App")
 
-HEIGHT = 650
-WIDTH = 800
 
 
-window = tk.Canvas(app, height=HEIGHT, width=WIDTH)
-window.pack()
-
-frame = tk.Frame(app, bg="#6cb5f9")
-frame.place(relheight=1, relwidth=1)
+userWindow = tk.Canvas(app)
+userWindow.pack()
 
 
-titlelabel = tk.Label(frame, text="Flight Reservation App")
-titlelabel.config(bg="#6cb5f9", fg="#FFFFFF", font="Garamond 25 bold")
-titlelabel.pack()
+# frame = tk.Frame(app)
+# frame.place(relheight=50, relwidth=50)
+
+
+
+# titlelabel = tk.Label(frame, text="Flight Reservation App")
+# titlelabel.config(bg="#FFFFFF", fg="#000000")
+# titlelabel.pack()
 
 
 # Label
-class label:
-    nameLabel = tk.Label(app, text="Name:")
-    nameLabel.place(x=630, y=70, anchor='w')
-    nameLabel.config(bg="#6cb5f9")
 
-    DOBLabel = tk.Label(app, text="Date of Birth:")
-    DOBLabel.place(x=630, y=100, anchor='w')
-    DOBLabel.config(bg="#6cb5f9")
+nameLabel = tk.Label(app, text="Name:")
+nameLabel.place(x=220, y=70, anchor='w')
+    
 
-    fromLabel = tk.Label(app, text="From:")
-    fromLabel.place(x=630, y=130, anchor='w')
-    fromLabel.config(bg="#6cb5f9")
+DOBLabel = tk.Label(app, text="Date of Birth:")
+DOBLabel.place(x=220, y=100, anchor='w')
+# # DOBLabel.config(bg="#6cb5f9")
+
+fromLabel = tk.Label(app, text="From:")
+fromLabel.place(x = 220, y=130, anchor='w')
+  
 
 
-    toLabel = tk.Label(app, text="To:")
-    toLabel.place(x=630, y=160, anchor='w')
-    toLabel.config(bg="#6cb5f9")
+toLabel = tk.Label(app, text="To:")
+toLabel.place(x=220, y=160, anchor='w')
+    
 
-    seatClassLabel = tk.Label(app, text="Seat Class:")
-    seatClassLabel.place(x=630, y=190, anchor='w')
-    seatClassLabel.config(bg="#6cb5f9")
+seatClassLabel = tk.Label(app, text="Seat Class:")
+seatClassLabel.place(x=220, y=190, anchor='w')
+    
 
-    classLabel = tk.Label(app, text="Class:")
-    classLabel.place(x=630, y=220, anchor='w')
-    classLabel.config(bg="#6cb5f9")
+classLabel = tk.Label(app, text="Class:")
+classLabel.place(x=220, y=220, anchor='w')
+    
 
-    seatLabel = tk.Label(app, text="Seat:")
-    seatLabel.place(x=630, y=250, anchor='w')
-    seatLabel.config(bg="#6cb5f9")
+seatLabel = tk.Label(app, text="Seat:")
+seatLabel.place(x=220, y=250, anchor='w')
+  
 
-    PIDLabel = tk.Label(app, text="PID:")
-    PIDLabel.place(x=630, y=280, anchor='w')
-    PIDLabel.config(bg="#6cb5f9")
+PIDLabel = tk.Label(app, text="PID:")
+PIDLabel.place(x=220, y=280, anchor='w')
+   
 
 
 # entry
 nameEntry = tk.Entry(app)
-window.create_window(500, 70, window=nameEntry)
+userWindow.create_window(90, 70, window=nameEntry)
 
 DOBEntry = tk.Entry(app)
-window.create_window(500, 100, window=DOBEntry)
+userWindow.create_window(90, 100, window=DOBEntry)
 
-
-# storing info
-
-# nameVar = StringVar(app)
-# DOBVar = StringVar(app)
-# fromVar = StringVar(app)
-# toVar = StringVar(app)
-# typeVar = StringVar(app)
-# seatClassVar = StringVar(app)
-# seatVar = StringVar(app)
-# PIDVar = StringVar(app)
 
 
 # combobox
@@ -89,28 +78,28 @@ window.create_window(500, 100, window=DOBEntry)
 fromCb = ttk.Combobox(app , width=17)
 fromCb['values'] = ['NEW YORK', 'HANOI', "TOKYO", "BANGKOK", "PARIS", "LONDON"]
 fromCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-fromCb.place(x=745, y=130, anchor='w')
-# fromCb.current(0)
+fromCb.place(x=335, y=130, anchor='w')
+
 
 
 toCb = ttk.Combobox(app, width=17)
 toCb['values'] = ['NEW YORK', 'HANOI', "TOKYO", "BANGKOK", "PARIS", "LONDON"]
 toCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-toCb.place(x=745, y=160, anchor='w')
-# toCb.current(0)
+toCb.place(x=335, y=160, anchor='w')
+
 
 typeCb = ttk.Combobox(app, width=17)
 typeCb['values'] = ['SILVER', 'GOLD', "PLATINIUM"]
 typeCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-typeCb.place(x=745, y=190, anchor='w')
-# typeCb.current(0)
+typeCb.place(x=335, y=190, anchor='w')
+
 
 
 seatClassCb = ttk.Combobox(app, width=17)
 seatClassCb['values'] = ['A', 'B', "C", "S"]
 seatClassCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-seatClassCb.place(x=745, y=220, anchor='w')
-# seatClassCb.current(0)
+seatClassCb.place(x=335, y=220, anchor='w')
+
 
 seatCb = ttk.Combobox(app, width=17)
 seatCb['values'] = ['111', '222', "333", "444", "555",
@@ -118,7 +107,7 @@ seatCb['values'] = ['111', '222', "333", "444", "555",
                     "1000", "1111", "2222", "3333", "4444", "5555",
                     "6666", "7777"]
 seatCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-seatCb.place(x=745, y=250, anchor='w')
+seatCb.place(x=335, y=250, anchor='w')
 
 
 PIDCb = ttk.Combobox(app, width=17)
@@ -128,54 +117,43 @@ PIDCb['values'] = ['VietNam Airline', 'Pacific Airlines', "VASCO", "VietJet Air"
                    "Air China", "Japan Airlines", "Air Canada", "Air France",
                    "Air India", "Asiana Airlines", "Aero Contractors"]
 PIDCb.pack(ipadx=1, ipady=1, side=BOTTOM)
-PIDCb.place(x=745, y=280, anchor='w')
-# PIDCb.current(0)
+PIDCb.place(x=335, y=280, anchor='w')
+
 
 
 def submit():
-    # name = nameVar.get()
-    # DOBs = DOBVar.get()
-    # fromPlace = fromVar.get()
-    # toPlace = toVar.get()
-    # type = typeVar.get()
-    # seatClass = seatClassVar.get()
-    # seat = seatVar.get()
-    # pID = PIDVar.get()
-    
-    
-    
+        # name = nameVar.get()
+        # DOBs = DOBVar.get()
+        # fromPlace = fromVar.get()
+        # toPlace = toVar.get()
+        # type = typeVar.get()
+        # seatClass = seatClassVar.get()
+        # seat = seatVar.get()
+        # pID = PIDVar.get()
+        
+        
+        
     name = nameEntry.get()
-    print(name)
-
     DOBs = DOBEntry.get()
-    print(DOBs)
-    
     fromPlace = fromCb.get()
-    print(fromPlace)
-    
     toPlace = fromCb.get()
-    print(toPlace)
-    
     type = typeCb.get()
-    print(type)
-    
     seatClass = seatClassCb.get()
-    print(seatClass)
-    
     seat = seatCb.get()
-    print(seat)
-    
     pID = PIDCb.get()
-    print(pID) 
+        
+    
+    
+        
+        
+    tv.insert(parent='', index=0, iid=1, values=(name,DOBs,fromPlace,toPlace,type,seatClass,seat,pID)) 
+    
 
-    user.append({'name': name , 'DOB': DOBs , 'from' : fromPlace, "To":toPlace, 'type' : type,
-                 'Seat Class': seatClass , 'Seat': seat, 'PID': pID })
+   
 
-    msg.showinfo("Your Information", "Your Information recorded")
     
     
 
-    
     
     
 
@@ -184,8 +162,80 @@ def submit():
 
     
 buttonSrc = tk.Button(app, text="Submit", command= submit)
-buttonSrc.place(x=745, y=330)
+
 buttonSrc.pack()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tv = ttk.Treeview(app)
+tv['columns']=('Name', 'DOBs','From','To','Seat Class', 'Class','Seat','PID')
+tv.column('#0', width=0, stretch=NO)
+# tv.column('Rank', anchor=CENTER, width=80)
+tv.column('Name', anchor=CENTER, width=100)
+tv.column('DOBs', anchor=CENTER, width=100)
+tv.column('From', anchor=CENTER, width=100)
+tv.column('To', anchor=CENTER, width=100)
+tv.column('Seat Class', anchor=CENTER, width=100)
+tv.column('Class', anchor=CENTER, width=100)
+tv.column('Seat', anchor=CENTER, width=100)
+tv.column('PID', anchor=CENTER, width=100)
+
+
+
+
+# tv.heading('#0', text='', anchor=CENTER)
+tv.heading('Name', text='Name', anchor=CENTER)
+tv.heading('DOBs', text='DOBs', anchor=CENTER)
+tv.heading('From', text='From', anchor=CENTER)
+tv.heading('To'  , text='To'  , anchor=CENTER)
+tv.heading('Seat Class', text='Seat Class', anchor=CENTER)
+tv.heading('Class', text='Class', anchor=CENTER)
+tv.heading('Seat', text = 'Seat' ,anchor=CENTER)
+tv.heading('PID',  text = "PID",anchor=CENTER)
+
+
+
+
+def delAllCustomer ():
+    for i in tv.get_children():
+        tv.delete(i)
+
+
+
+
+
+btnDelAll = tk.Button(app, text = "Delete All",command = delAllCustomer)
+btnDelAll.pack()
+
+
+
+
+
+
+
+
+
+
+tv.pack()
+
 app.mainloop()
-print(user[0])
